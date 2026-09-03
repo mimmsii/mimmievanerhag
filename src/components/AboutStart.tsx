81340e1733e94f492b8125c02e846f2a1d21f488
Mimmie Vänerhag <mimmie.vanerhag@gmail.com>
2026-09-03 11:07:49 +0200

    small fix

--- src/components/AboutStart.tsx
+++ src/components/AboutStart.tsx
@@ -1,19 +1,19 @@
 export default function AboutStart() {
   return (
     <section className="pt-24 md:pt-32 md:pb-4">
-      <div className="max-w-5xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-16">
+      <div className="max-w-5xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-16">
         
         <p className="text-lg font-sans leading-[1.6] md:leading-normal text-charcoal">
           Som frontendutvecklare fokuserar jag på att bygga genomtänkta och
           användarvänliga gränssnitt där design och funktion samspelar.
-          <span className="block md:inline pt-2 md:pt-0"> Jag är
+          <span className="block md:inline pt-1 md:pt-0"> Jag är
           ständigt nyfiken på nya verktyg och arbetssätt som kan förbättra både
           utvecklingsprocessen och slutprodukten.</span>
         </p>
 
         <p className="text-lg font-sans leading-[1.6] md:leading-normal text-charcoal">
           Med en bakgrund inom entreprenörskap har jag också ett starkt affärsperspektiv i det jag utvecklar. 
-          <span className="block md:inline pt-2 md:pt-0"> För mig handlar en bra digital lösning inte bara om hur den byggs, utan också om varför den behövs, 
+          <span className="block md:inline pt-1 md:pt-0"> För mig handlar en bra digital lösning inte bara om hur den byggs, utan också om varför den behövs, 
           vem som ska använda den och vilket värde den ska skapa.</span>
         </p>
 
