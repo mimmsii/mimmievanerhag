589febe3021f6c612d1e1364a6af1695e7812917
Mimmie Vänerhag <mimmie.vanerhag@gmail.com>
2026-09-04 08:56:41 +0200

    small fix padding start

--- src/components/AboutStart.tsx
+++ src/components/AboutStart.tsx
@@ -6,14 +6,14 @@ export default function AboutStart() {
         <p className="text-lg font-sans leading-[1.6] md:leading-normal text-charcoal">
           Som frontendutvecklare fokuserar jag på att bygga genomtänkta och
           användarvänliga gränssnitt där design och funktion samspelar.
-          <span className="block md:inline pt-2 md:pt-0"> Jag är
+          <span className="block md:inline pt-1 md:pt-0"> Jag är
           ständigt nyfiken på nya verktyg och arbetssätt som kan förbättra både
           utvecklingsprocessen och slutprodukten.</span>
         </p>
 
         <p className="text-lg font-sans leading-[1.6] md:leading-normal text-charcoal">
           Med en bakgrund inom entreprenörskap har jag också ett starkt affärsperspektiv i det jag utvecklar. 
-          <span className="block md:inline pt-2 md:pt-0"> För mig handlar en bra digital lösning inte bara om hur den byggs, utan också om varför den behövs, 
+          <span className="block md:inline pt-1 md:pt-0"> För mig handlar en bra digital lösning inte bara om hur den byggs, utan också om varför den behövs, 
           vem som ska använda den och vilket värde den ska skapa.</span>
         </p>
 
